49705bf51a1189cbde9454fd89c24956313c1aae
Ging Siyaphat <siyaphat.a12@gmail.com>
2025-01-14 22:56:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -156,7 +156,7 @@ function App() {
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="#">Siyaphat</a>.
       </div>
     </>
   );
